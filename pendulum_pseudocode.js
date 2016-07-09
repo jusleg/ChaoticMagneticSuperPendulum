19fb0a7f016aaafb6732d164ed881e911ad67a2a
felleg <felleg@users.noreply.github.com>
2016-07-09 16:05:19 -0400

    clarified variable names

--- pendulum_pseudocode.js
+++ pendulum_pseudocode.js
@@ -1,18 +1,18 @@
 // Pendulum variables
-var s;	// 2D vector, Position of the pendulum at all times
-var v;	// 2D vector, Velocity of the pendulum at all times
-var a;	// 2D vector, Acceleration of the pendulum at all times
-var pol;		// Pendulum magnet's polarity (A/m)
+var position;	// 2D vector, Position of the pendulum at all times
+var velocity;	// 2D vector, Velocity of the pendulum at all times
+var acceleration;	// 2D vector, Acceleration of the pendulum at all times
+var polarity;		// Pendulum magnet's polarity (A/m)
 var start_coord;	// 2D vector, Coordinates of the start point (from where pendulum is dropped)
 
 // Structure of magnets object 
-var s_m_n;	// 2D vector, Position of the n-th magnet
-var m_pol_n;	// int (-1 or +1), polarity of the n-th magnet. NOTE: could be included inside magnet vector to make it a 3D vector.
+var position_magnet_n;	// 2D vector, Position of the n-th magnet
+var polarity_magnet_n;	// int (-1 or +1), polarity of the n-th magnet. NOTE: could be included inside magnet vector to make it a 3D vector.
 
 // Values that vary with a slider
-var m;		// Pendulum mass (kg)
-var str;	// Strength of all magnets (they vary in unison) (A/m)
-var h;		// Height of pendulum plane above magnets plane (m)
+var mass;		// Pendulum mass (kg)
+var strength_magnets;	// Strength of all magnets (they vary in unison) (A/m)
+var height;		// Height of pendulum plane above magnets plane (m)
 var k_h;	// Hooke law spring constant, acts as gravity (kg/s^2)
 var k_f;	// Friction force coefficient (kg/s)
 
@@ -34,8 +34,8 @@ var delta_t=0.01;		// Value of a step forward in time (s)
 main() {
 /*-------- Load a template -------*/
 for ("all magnets from a template") {
-	s_m_n = "value from template";
-	pol_n = "value from template";
+	position_magnet_n = "value from template";
+	polarity_magnet_n = "value from template";
 }
 
 /* We let user change values with sliders, maybe add more magnets,
@@ -53,8 +53,8 @@ run() {
 	/*-------- Initialize ------*/
 
 	// Pendulum
-	s = start_coord;
-	v = 0;
+	position = start_coord;
+	velocity = 0;
 
 
 	/*-------- Start moving pendulum ------*/
@@ -70,16 +70,16 @@ run() {
 		// Magnetic force
 		var F_m_tot;	// Total magnetic force
 		for ("all magnets on the plane") {
-			var R = sqrt( (s.x)^2 + (s.y)^2 );	// Distance between pendulum and center of the plane
-			var r = sqrt( (s_m_n.x)^2 + (s_m_n.y)^2 );	// Distance between n-th magnet and center of the plane
+			var R = sqrt( (position.x)^2 + (position.y)^2 );	// Distance between pendulum and center of the plane
+			var r = sqrt( (position_magnet_n.x)^2 + (position_magnet_n.y)^2 );	// Distance between n-th magnet and center of the plane
 			var d = sqrt(h^2+r^2+R^2);	// Distance between pendulum and n-th magnet
 			
 			// Sum contribution of all magnets into the total force (2D vector addition)
-			F_m_tot += MU * pol * m_pol_n * str^2 / (4 * PI * d^2);
+			F_m_tot += MU * polarity * polarity_magnet_n * strength_mgnet^2 / (4 * PI * d^2);
 		}
-		a = (F_g + F_f + F_m_tot) / m;	// Newton's second law to get instant pendulum acceleration
-		v = v + a*t;	// Pendulum's velocity is modified due to acceleration
-		s = s + v*t + 0.5*a*t^2;	// Position of pendulum is modified
+		acceleration = (F_g + F_f + F_m_tot) / mass;	// Newton's second law to get instant pendulum acceleration
+		velocity = velocity + acceleration*t;	// Pendulum's velocity is modified due to acceleration
+		position = position + velocity*t + 0.5*acceleration*t^2;	// Position of pendulum is modified
 		
 		canvas.("update pendulum s position")
 		t+=delta_t;	// Go forward in time by one step
